3a14579841827804720c38a230180741c17ad570
imrany <imranmat254@gmail.com>
2024-06-02 14:28:54 +0300

    UI update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/Link"
+import { FaGithub } from "react-icons/fa";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -35,7 +36,9 @@ export default function RootLayout({
                     <Link href="/blog">Blog</Link>
                 </div>
                 <div className="flex items-center gap-x-5">
-                    <a href="https://github.com/imrany/anvel" target="_blank" rel="noopener noreferrer">Github</a>
+                    <a href="https://github.com/imrany/anvel" target="_blank" rel="noopener noreferrer">
+                        <FaGithub className="text-xl text-gray-500 hover:text-black"/>
+                    </a>
                 </div>
             </div>
         </nav>
